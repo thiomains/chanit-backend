9c06583e8d689b8c5482b127a47d0a76547a343b
thiomains <thiomains@icloud.com>
2025-08-10 17:47:42 +0200

    added login name to user profile response in /auth/me endpoint

--- endpoints/auth/me.js
+++ endpoints/auth/me.js
@@ -1,8 +1,11 @@
 const profiles = require('./../../profiles')
+const users = require('../../users')
 
 async function me(req, res) {
 
     const profile = await profiles.getProfile(req.auth.user.id)
+    const user = await users.getUser(req.auth.user.id)
+    profile.loginName = user.username
 
     res.status(200).send(profile);
 
