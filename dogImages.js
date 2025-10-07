c8acf6c3e1eaf3b01cd79fb9610956ee8c88037b
Thimo Colditz <thiomains@icloud.com>
2025-10-07 14:27:42 +0200

    Update randomDogImage to use CDN for dog image URLs

--- dogImages.js
+++ dogImages.js
@@ -3,7 +3,8 @@ const axios = require("axios");
 async function randomDogImage() {
     const dogApi = "https://dog.ceo/api/breeds/image/random"
     const res = await axios.get(dogApi)
-    return res.data.message;
+    const original = res.data.message;
+    return original.replace("https://images.dog.ceo/breeds/", "https://cdn.faser.app/dog-api/");
 }
 
 module.exports = { randomDogImage }
