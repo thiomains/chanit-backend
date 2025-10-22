6426561b24bbb4e72ec736517c77e351c7d21ece
thiomains <thiomains@icloud.com>
2025-10-22 02:59:01 +0200

    feat: add specific user information to admin users endpoint

--- endpoints/admin/users.js
+++ endpoints/admin/users.js
@@ -1,8 +1,26 @@
 const users = require("../../users")
+const profiles = require("../../profiles")
+const friends = require("../../friends")
+const sessions = require("../../sessions")
 
 async function get(req, res) {
-    const allUsers = await users.getAllUsers()
-    res.send(allUsers)
+    const userId = req.query.userId
+    if (!userId) {
+        const allUsers = await users.getAllUsers()
+        res.send(allUsers)
+        return
+    }
+    const user = await users.getUser(userId)
+    const userProfile = await profiles.getProfile(userId)
+    const userFriends = await friends.getFriends(userId)
+    const userSessions = await sessions.getSessions(userId)
+    user.profile = userProfile
+    user.friends = userFriends
+    user.sessions = userSessions
+
+    delete user.password
+
+    res.send(user)
 }
 
 module.exports = { get }
